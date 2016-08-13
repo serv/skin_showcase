2693a01aee4ca93dd5f285b6b726b54a35f74d0d
Jason Kim <iamjsonkim@gmail.com>
2016-08-12 18:09:04 -0700

    basic unit test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'attributes (persistent)' do
+    user = User.new
+    assert_equal user.username, nil
+    assert_equal user.salted_password, nil
+    assert_equal user.email, nil
+    assert_equal user.ref_owned_skins, nil
+    assert_equal user.ref_wanted_skins, nil
+    assert_equal user.ref_ratings, nil
+    assert_equal user.ref_comments, nil
+    assert_equal user.ref_friends, nil
+  end
+
+  test 'attributes (transient)' do
+    user = User.new
+    assert_equal user.password_confirmation, nil
+  end
 end
