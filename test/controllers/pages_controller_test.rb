70573dffbfd51917a49a88eb23f3493131161237
Jason Kim <iamjsonkim@gmail.com>
2016-09-10 23:27:17 -0700

    chore: test pages (#28)

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should get /' do
+    get root_url
+    assert_response :success
+  end
+
+  test 'should get /about' do
+    get about_url
+    assert_response :success
+  end
+
+  test 'should get /contact' do
+    get contact_url
+    assert_response :success
+  end
+
+  test 'should get /user_agreement' do
+    get user_agreement_url
+    assert_response :success
+  end
+
+  test 'should get /privacy' do
+    get privacy_url
+    assert_response :success
+  end
 end
