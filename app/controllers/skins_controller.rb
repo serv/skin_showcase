0b2fae5fbf5d76deae3edc29b6c91fae3c687266
Jason Kim <iamjsonkim@gmail.com>
2016-09-17 21:58:16 -0700

    bug: ensure that logged out user can visit skin page (#40)

--- app/controllers/skins_controller.rb
+++ app/controllers/skins_controller.rb
@@ -7,11 +7,13 @@ class SkinsController < ApplicationController
       skinId: @skin.id
     }
 
+    existing_review = nil
     if current_user
       existing_review = current_user.review_for(@skin)
-      @review = existing_review ? existing_review : @skin.reviews.build
       @rating = current_user.rating_for(@skin)
       @global[:rating] = @rating if @rating
     end
+
+    @review = existing_review ? existing_review : @skin.reviews.build
   end
 end
