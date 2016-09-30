3173bc6f2e4e6a879f8b6e27c12f79d5c9bb6d37
Jason Kim <iamjsonkim@gmail.com>
2016-09-29 23:43:48 -0700

    chore: associate rating and review (#49)

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -6,4 +6,14 @@ class Rating < ApplicationRecord
   validates :skin_id, presence: true
   validates :user_id, presence: true
   validates :score, presence: true
+
+  before_save :associate_review
+
+  private
+
+    # When a rating is saved, check if there is a review to associate with
+    def associate_review
+      review = Review.find_by(user_id: self.user_id, skin_id: self.skin_id)
+      self.review_id = review.id if review
+    end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,11 +2,14 @@ class Review < ApplicationRecord
   belongs_to :skin
   belongs_to :user
   has_many :helpfuls
+  has_one :rating
 
   validates :skin_id, presence: true
   validates :user_id, presence: true
   validates :title, presence: true
   validates :long_form, presence: true
+
+  after_save :associate_rating
 
   def total_helpfuls
     helpfuls
@@ -15,4 +18,13 @@ class Review < ApplicationRecord
   def total_up_helpfuls
     helpfuls.select { |h| h.upvote == 1 }
   end
+
+  private
+
+    # When a review is saved, check if there is a rating to associate with
+    def associate_rating
+      rating = Rating.find_by(user_id: self.user_id, skin_id: self.skin_id)
+      puts self.inspect
+      rating.update_attribute('review_id', self.id) if rating
+    end
 end
